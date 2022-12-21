5294c127039b2ea4fde5eb5cb4e8fa8a2e20ba1f
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2022-12-21 09:56:20 +0530

    Adding some trick solution

--- Leetcode Problems/Reverse an array.java
+++ Leetcode Problems/Reverse an array.java
@@ -1,6 +1,7 @@
 package Questions;
 
 public class reverse_array {
+    //Simple Reversing.
     static void Reverse(int arr[] , int n){
             int start = 0;
             int end = n-1;
@@ -14,27 +15,30 @@ public class reverse_array {
                 end--;
             }
     }
-
-    public static void main(String[] args) {
-        int arr[] = {2,9,8,6,5,20};
-        int n = arr.length;
-
-        for(int i = 0; i < n; i++)
-        {
-            System.out.print(arr[i]+" ");
-        }
-
-        System.out.println();
-
-
-
-
-        System.out.println("After Reverse");
-        Reverse(arr, n);
-
-        for(int i = 0; i < n; i++)
-        {
-            System.out.print(arr[i]+" ");
+    
+    //Here some trick - we get a target 'm' through which we have to reverse the array after the 'm' number of size.
+    
+    // for ex:
+    // arr[] = {1,2,3,4,5};
+    // getting m = 2;
+    
+    // so we have to reverse the array after the array size 2 , i.e
+    //{4,5};
+    
+    public static void reverseArray(ArrayList<Integer> arr, int m)
+    {
+        // Write your code here.
+        int i = m+1;
+        int j = arr.size() - 1;
+        while (i < j){
+            int temp = arr.get(i);
+            arr.set(i,arr.get(j));
+            arr.set(j,temp);
+            j--;
+            i++;
         }
     }
+
 }
+
+Thanks 😊
